d5d93a4f5208916b3aa36cec765a005fed486462
Abhishek-Caleb <abhishek.caleb@westpac.com.au>
2026-03-04 22:10:07 +1100

    fix: convert Constants.js to ESM exports, replace spreadsheet config with EDS-derived CSS URL

--- src/universal-editor-ui-1/web-src/src/components/Constants.js
+++ src/universal-editor-ui-1/web-src/src/components/Constants.js
@@ -1,13 +1,16 @@
 /*
-* <license header>
-*/
+ * <license header>
+ */
 
-module.exports = {
-  extensionId: 'wp-custpm-rte-styles',
-  RICHTEXT_TYPE: 'richtext',
-  BROADCAST_CHANNEL_NAME: 'wp-events-channel',
-  EVENT_AUE_UI_SELECT: 'aue:ui-select',
-  UNIVERSAL_EDITOR_CONFIG_SPREADSHEET: 'rte-styles',
-  RTE_STYLES_URL: 'RTE_STYLES_URL',
-  EVENT_AUE_UI_UPDATE: 'aue:content-update'
-}
+export const extensionId = 'wp-custpm-rte-styles';
+export const RICHTEXT_TYPE = 'richtext';
+export const BROADCAST_CHANNEL_NAME = 'wp-events-channel';
+export const EVENT_AUE_UI_SELECT = 'aue:ui-select';
+export const EVENT_AUE_UI_UPDATE = 'aue:content-update';
+
+// EDS GitHub repo details for constructing the CSS URL from editorState
+export const EDS_GITHUB_ORG = 'westpac-eds-program';
+export const EDS_GITHUB_REPO = 'westpac';
+
+// Path to the RTE styles CSS file in the EDS repo
+export const RTE_STYLES_CSS_PATH = '/styles/rte-styles.css';
