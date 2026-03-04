7a8afbd1bcb8cd9f1cec67ae91888f663567a162
Abhishek Caleb <abhishek.caleb@westpac.com.au>
2026-03-04 21:42:34 +1100

    update constants

--- src/universal-editor-ui-1/web-src/src/components/Constants.js
+++ src/universal-editor-ui-1/web-src/src/components/Constants.js
@@ -3,5 +3,11 @@
 */
 
 module.exports = {
-  extensionId: 'wp-custpm-rte-styles'
+  extensionId: 'wp-custpm-rte-styles',
+  RICHTEXT_TYPE: 'richtext',
+  BROADCAST_CHANNEL_NAME: 'wp-events-channel',
+  EVENT_AUE_UI_SELECT: 'aue:ui-select',
+  UNIVERSAL_EDITOR_CONFIG_SPREADSHEET: 'universal-editor-config',
+  RTE_STYLES_URL: 'RTE_STYLES_URL',
+  EVENT_AUE_UI_UPDATE: 'aue:content-update'
 }
